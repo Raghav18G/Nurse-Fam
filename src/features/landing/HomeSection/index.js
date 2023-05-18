633e18cdc22495857210864847ff600dd1390681
unknown <raghavgoel847@gmail.com>
2023-05-18 17:10:43 +0530

    Landing Additions

--- src/features/landing/HomeSection/index.js
+++ src/features/landing/HomeSection/index.js
@@ -1,9 +1,96 @@
-import React from 'react'
-
+import React from "react";
+import "../styles.css";
+import logo from "../../../image/signup/logo.png";
+import { Button, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 const HomeSection = () => {
+  const navigate = useNavigate();
   return (
-    <div>HomeSection</div>
-  )
-}
+    <div className="HomeSection--Container">
+      <div className="HomeSection--Navbar">
+        <img src={logo} style={{ width: "200px" }} />
+        <div className="HomeSection--Navbar--text">
+          <Typography
+            sx={{
+              fontWeight: "600",
+              color: "#666F80",
+              fontFamily: "Montserrat",
+              textAlign: "center",
+              margin: "1rem",
+              cursor: "pointer",
+            }}
+          >
+            Find Jobs
+          </Typography>
+          <Typography
+            sx={{
+              fontWeight: "800",
+              color: "#666F80",
+              fontFamily: "Montserrat",
+              textAlign: "center",
+              margin: "1rem",
+              cursor: "pointer",
+            }}
+          >
+            .
+          </Typography>
+          <Typography
+            sx={{
+              fontWeight: "600",
+              color: "#666F80",
+              fontFamily: "Montserrat",
+              textAlign: "center",
+              margin: "1rem",
+              cursor: "pointer",
+            }}
+          >
+            Find Care
+          </Typography>
+          <Typography
+            sx={{
+              fontWeight: "800",
+              color: "#666F80",
+              fontFamily: "Montserrat",
+              textAlign: "center",
+              margin: "1rem",
+              cursor: "pointer",
+            }}
+          >
+            .
+          </Typography>
+          <Typography
+            sx={{
+              fontWeight: "600",
+              color: "#666F80",
+              fontFamily: "Montserrat",
+              textAlign: "center",
+              margin: "1rem",
+              cursor: "pointer",
+            }}
+          >
+            For Employers
+          </Typography>
+        </div>
+        <div style={{ marginTop: "-2rem" }}>
+          <Typography
+            sx={{
+              textAlign: "end",
+              marginBottom: "1rem",
+              color: "#A52B92",
+              fontWeight: "800",
+              cursor: "pointer",
+            }}
+            onClick={() => navigate("/login")}
+          >
+            Sign In
+          </Typography>
+          <Button variant="contained" sx={{ marginTop: "-0.5rem" }}>
+            Partner Program
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default HomeSection
+export default HomeSection;
