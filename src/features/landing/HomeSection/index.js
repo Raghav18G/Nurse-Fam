6a5f8f8992450c5320f5d1a96522f4f6d26317a0
unknown <raghavgoel847@gmail.com>
2023-05-18 18:24:35 +0530

    Home Component

--- src/features/landing/HomeSection/index.js
+++ src/features/landing/HomeSection/index.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles.css";
 import logo from "../../../image/signup/logo.png";
-import { Button, Typography } from "@mui/material";
+import {
+  Autocomplete,
+  Button,
+  Paper,
+  TextField,
+  Typography,
+  createFilterOptions,
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
+
+const filter = createFilterOptions();
+
 const HomeSection = () => {
   const navigate = useNavigate();
+  const [value, setValue] = useState(null);
   return (
     <div className="HomeSection--Container">
       <div className="HomeSection--Navbar">
@@ -87,6 +98,103 @@ const HomeSection = () => {
           <Button variant="contained" sx={{ marginTop: "-0.5rem" }}>
             Partner Program
           </Button>
+        </div>
+      </div>
+      <div className="HomeSection--MainContent">
+        <Typography
+          variant="h4"
+          sx={{
+            fontWeight: "800",
+            width: "40rem",
+            color: "#fff",
+            marginTop: "5rem",
+            textAlign: "center",
+          }}
+        >
+          Trusted and Verified Professional Global Care Network
+        </Typography>
+        <Typography
+          variant="body1"
+          sx={{
+            width: "25rem",
+            color: "#fff",
+            marginTop: "2rem",
+            textAlign: "center",
+          }}
+        >
+          Find your dream job and Connect with Top Employers in the Care
+          Industry
+        </Typography>
+        <div className="HomeSection--Paper">
+          <Paper elevation={3} sx={{ height: "6rem", width: "35rem" }}>
+            <div className="HomeSection--Paper--Content">
+              <Autocomplete
+                value={value}
+                style={{ fontWeight: "800", color: "#666F80" }}
+                onChange={(event, newValue) => {
+                  if (typeof newValue === "string") {
+                    setValue({
+                      title: newValue,
+                    });
+                  } else if (newValue && newValue.inputValue) {
+                    // Create a new value from the user input
+                    setValue({
+                      title: newValue.inputValue,
+                    });
+                  } else {
+                    setValue(newValue);
+                  }
+                }}
+                filterOptions={(options, params) => {
+                  const filtered = filter(options, params);
+
+                  const { inputValue } = params;
+                  // Suggest the creation of a new value
+                  const isExisting = options.some(
+                    (option) => inputValue === option.title
+                  );
+                  if (inputValue !== "" && !isExisting) {
+                    filtered.push({
+                      inputValue,
+                      title: `Add "${inputValue}"`,
+                    });
+                  }
+
+                  return filtered;
+                }}
+                selectOnFocus
+                clearOnBlur
+                handleHomeEndKeys
+                id="free-solo-with-text-demo"
+                options={[]}
+                getOptionLabel={(option) => {
+                  // Value selected with enter, right from the input
+                  if (typeof option === "string") {
+                    return option;
+                  }
+                  // Add "xxx" option created dynamically
+                  if (option.inputValue) {
+                    return option.inputValue;
+                  }
+                  // Regular option
+                  return option.title;
+                }}
+                renderOption={(props, option) => (
+                  <li {...props}>{option.title}</li>
+                )}
+                sx={{ width: 300 }}
+                freeSolo
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Search for jobs..."
+                    sx={{ fontWeight: "800", color: "#666F80" }}
+                    // sx={{ borderRadius: "20px", border: "1px solid #A52B92" }}
+                  />
+                )}
+              />
+            </div>
+          </Paper>
         </div>
       </div>
     </div>
