6129817e47fbd0766addcfeb5c36b62fbcb9e29c
unknown <raghavgoel847@gmail.com>
2023-05-18 17:33:14 +0530

    Latest Commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { adminRoleGetterService } from "./shared/services";
 
-import Login from "./features/auth";
 import PrivateRoute from "./features/routes/PrivateRoute";
 import PublicRoute from "./features/routes/PublicRoute";
 import SidebarLayout from "./features/side-bar/SidebarLayout";
