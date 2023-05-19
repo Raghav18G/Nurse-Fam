8efbf8cf15e3d79e07d663da40304b66054c1250
sananvanshika <vanshika.s@antino.io>
2023-05-19 11:28:45 +0530

    Footer

--- src/features/landing/FooterSection/index.js
+++ src/features/landing/FooterSection/index.js
@@ -1,10 +1,21 @@
 import React from "react";
+import logo from "../../../image/FooterLogoImage.png";
 import "../styles.css";
+import { Divider, Grid } from "@mui/material";
 
 const FooterSection = () => {
-  return <div>
-    
-  </div>;
+  return (
+    <div className="landingFooterContainer">
+      <div className="landingFooterLogo">
+        <img src={logo} style={{ width: "200px" }} />
+      </div>
+      <div className="landingFooterLogo">
+      Terms & Conditions . Privacy Policies . Sitemap 
+      <br />
+      &#169; MedFam 2023 All Rights Reserved.
+      </div>
+    </div>
+  );
 };
 
 export default FooterSection;
